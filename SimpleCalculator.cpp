5b907423b71ea86d11a6204240ae1264e5a349b5
iNFiENiTE-World <infienite@gmail.com>
2022-05-07 21:09:02 +0800

    Simple calculator is functioning! All operations (+,-,x,/) is working as intended. The precedence of operations work flawlessly! Though I must say that the debugging process is tiring due to silent runtime error :(. I must admit, C++ is fast, the program run is blazingly faster than my experience in other languages. So, the code is not well-structured. I must documentate this program since this is my first and I must do more refactoring and tweak with its philosophy a bit to make this program more consistent and extensible. The code is spaghetti I know...

--- SimpleCalculator.cpp
+++ SimpleCalculator.cpp
@@ -1,6 +1,6 @@
 # include <iostream>
 # include <cmath>
-# define DELIMITER ','
+char DELIMITER = ',';
 using namespace std;
 
 template <typename T>
@@ -23,14 +23,19 @@ class Stack {
         }
 
         T top () {
+            if (isEmpty()) {
+                return ' ';
+            }
             return stackTop -> data;
         }
 
-        void pop () {
+        T pop () {
             if (isEmpty()) {
-                return;
+                return ' ';
             }
+            T data = top();
             stackTop = stackTop -> next;
+            return data;
         }
 
         int isEmpty() {
@@ -46,6 +51,7 @@ class Calculator {
         };
 
         Stack<char> opers;
+        Stack<int> numbers;
 
         string standardExpression (string postfixExpression) {
             string infixExpression;
@@ -60,30 +66,37 @@ class Calculator {
 
                 if (!isOperator(singleExpression)) {
                     if (currentState != number) {
-                        infixExpression += DELIMITER + singleExpression;
+                        infixExpression += DELIMITER;
+                        infixExpression += singleExpression;
                         currentState = number;
                     }
-                    infixExpression += singleExpression;
+                    else {
+                        infixExpression += singleExpression;
+                    }
+
                     continue;
                 }
+
+                currentState = oper;
 
                 if (isPrecedence(singleExpression)) {
                     opers.push(singleExpression);
                     continue;
                 }
 
+                char op;
                 do {
-                    singleExpression = opers.top();
-                    infixExpression += DELIMITER + singleExpression;
-                    opers.pop();
-                } while (isSimilarPrecedence(singleExpression));
+                    op = opers.pop();
+                    infixExpression += DELIMITER;
+                    infixExpression += op;
+                } while (isSimilarPrecedence(op));
 
-                currentState = oper;
+                opers.push(singleExpression);
             }
 
             while (!opers.isEmpty()) {
-                infixExpression += opers.top();
-                opers.pop();
+                infixExpression += DELIMITER;
+                infixExpression += opers.pop();
             }
 
             return infixExpression;
@@ -100,6 +113,9 @@ class Calculator {
         }
 
         int isPrecedence (char oper) {
+            if (opers.isEmpty()) {
+                return 1;
+            }
             return getPriority(oper) >= getPriority(opers.top());
         }
 
@@ -119,9 +135,77 @@ class Calculator {
             return priority;
         }
 
+        int stringToInt (string number) {
+            int total = 0;
+
+            int size = number.length();
+            for (int i = size - 1; i != -1; i--) {
+                int digit = int(number[i]) - 48;
+                int value = digit * pow(10, size - i - 1);
+                total += value;
+            }
+
+            return total;
+        }
+
+        int operate (char expression) {
+            int x, y;
+            switch (expression) {
+                case '+':
+                    y = numbers.pop();
+                    x = numbers.pop();
+                    numbers.push(add(x, y));
+                    return 1;
+                case '-':
+                    y = numbers.pop();
+                    x = numbers.pop();
+                    numbers.push(subtract(x, y));
+                    return 1;
+                case 'x':
+                    y = numbers.pop();
+                    x = numbers.pop();
+                    numbers.push(multiply(x, y));
+                    return 1;
+                case '/':
+                    y = numbers.pop();
+                    x = numbers.pop();
+                    numbers.push(divide(x, y));
+                    return 1;
+                default:
+                    return 0;
+            }
+        }
+
     public:
         int calculate (string expression) {
+            string standardE = standardExpression(expression);
+            string subE;
+            for (int i = 0; i < standardE.length(); i++) {
 
+                char atomE = standardE[i];
+
+                if (atomE != ',') {
+                    subE += atomE;
+                    continue;
+                }
+
+                if (isOperator(subE[0])) {
+                    operate(subE[0]);
+                    subE = "";
+                    continue;
+                }
+
+                int num = stringToInt(subE);
+
+                numbers.push(num);
+
+                subE = "";
+            }
+
+            operate(subE[0]);
+
+            int numcout = numbers.pop();
+            return numcout;
         }
 
         
@@ -147,109 +231,14 @@ class Calculator {
 
 };
 
-// Stack
-
-# define SIZE 50
-
-using namespace std;
-
-int myStack[SIZE], stackTop, answer = 0;
-
-void display () {    
-    if (isEmptyStack()) {
-        cout << "Stack underflow" << endl;
-        return;
-    }
-
-    int i;
-
-    for (i = stackTop; i != -1; i--) {
-        cout << myStack[i] << " ";
-    }
-
-    cout << endl;
-}
-
-// Calculator functions
-// Expression Handler
-
-
-int operate (char expression) {
-    int x, y;
-    switch (expression) {
-        case '+':
-            y = pop();
-            x = pop();
-            push(add(x, y));
-            return 1;
-        case '-':
-            y = pop();
-            x = pop();
-            push(subtract(x, y));
-            return 1;
-        case 'x':
-            y = pop();
-            x = pop();
-            push(multiply(x, y));
-            return 1;
-        case '/':
-            y = pop();
-            x = pop();
-            push(divide(x, y));
-            return 1;
-        default:
-            return 0;
-    }
-}
-
-int stringToInt (string number) {
-    int total = 0;
-
-    int size = number.length();
-    for (int i = size - 1; i != -1; i--) {
-        int digit = int(number[i]) - 48;
-        int value = digit * pow(10, size - i - 1);
-        total += value;
-    }
-
-    return total;
-}
-
 int main () {
+    Calculator calculator;
     string expression;
-    initStack();
 
-    // Handle infix expression
+    cout << "Input mathematical expression" << endl;
     while (1) {
-        cout << "Input Number / Operator: ";
         cin >> expression;
 
-        if (expression[0] == 'q') {
-            exit(0);
-            continue;
-        }
-
-        string subExpression = "";
-
-        for (int i = 0; i < expression.length(); i++) {
-
-            if (expression[i] != ' ') {
-                subExpression += expression[i];
-                continue;
-            }
-
-            int isOperator = operate(subExpression[0]);
-                
-            if (isOperator) {
-                continue;
-            }
-
-            int num = stringToInt(subExpression);
-
-            push(num);
-        }
-
-        cout << pop() << endl;
-        initStack();
+        cout << calculator.calculate(expression) << endl;
     }
 }
